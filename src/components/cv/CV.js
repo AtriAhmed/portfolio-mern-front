caeb54cdd5f53cf73cc7b410a96684eeedd3012c
Ahmed Atri <atriahmed.1999@gmail.com>
2023-06-17 18:22:35 +0100

    nothing

--- src/components/cv/CV.js
+++ src/components/cv/CV.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer'
 import Documentt from './Documentt'
 
